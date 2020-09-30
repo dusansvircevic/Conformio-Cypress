36b63c20bb5b07a15443860a317751f65ec14d60
Dusan <68560679+dusansvircevic@users.noreply.github.com>
2020-09-30 16:43:33 +0200

    Update onboarding.spec.js

--- cypress/integration/onboarding/onboarding.spec.js
+++ cypress/integration/onboarding/onboarding.spec.js
@@ -1,102 +1,122 @@
-import {firstName, lastName, fullName, password, selector} from '../../fixtures/data.json'
+import credentials from '../../fixtures/credentials.json'
+import locators from '../../fixtures/locators.json'
 import {randomEmail, randomAlphabeticalString} from '../../utils/index.js'
 
 
-describe('Conformio Register, Login and Onboarding', () => {
+describe('Conformio Onboarding', () => {
 
   before(() => {
-    cy.visit('/')
+    cy.visit('/preonboarding/language')
   })
 
-  it('Register', () => {
-    //Izbor jezika
-    cy.get(selector.heading1).contains('Welcome to Conformio!').should('be.visible')
-    cy.get(selector.heading4).contains('Interface language').should('be.visible')
-    cy.get(selector.language).select('English')
-    cy.get(selector.submit).click()
-
-    //Email, Ime, Prezime, Password, Password confirmation
-    cy.get(selector.heading4).contains('Email address').should('be.visible')
-    cy.get(selector.heading4).contains('First name').should('be.visible')
-    cy.get(selector.heading4).contains('Last name').should('be.visible')
-    cy.get(selector.heading4).contains('Password').should('be.visible')
-    cy.get(selector.heading4).contains('Retype Password').should('be.visible')
-    cy.get(selector.submit).should('be.visible')
-    cy.get(selector.email).eq('0').type(randomEmail)
-    cy.get(selector.firstName).eq('1').type(firstName)
-    cy.get(selector.lastName).eq('2').type(lastName)
-    cy.get(selector.password).eq('3').type(password)
-    cy.get(selector.passwordConfirmation).eq('4').type(password)
-    cy.get(selector.submit).click()
-    cy.wait(3000)
-    
-    //Ime kompanije
-    cy.get(selector.heading4).contains('Company name').should('be.visible')
-    cy.get(selector.companyName).type(randomAlphabeticalString)
-    cy.get(selector.submit).click()
+  it('Registration - Language', () => {
+    cy
+      .get(locators.REGISTRATION.TITLE).contains('Interface language')
+        .should('be.visible')
+      .get(locators.REGISTRATION.LANGUAGE).select('English')
+      .get(locators.REGISTRATION.SUBMIT).click()
   })
 
+  it('Registration- Credentials', () => {
+    cy
+      .get(locators.REGISTRATION.INPUTNAME).contains('Email address')
+        .should('be.visible')
+      .get(locators.REGISTRATION.INPUTNAME).contains('First name')
+        .should('be.visible')
+      .get(locators.REGISTRATION.INPUTNAME).contains('Last name')
+        .should('be.visible')
+      .get(locators.REGISTRATION.INPUTNAME).contains('Password')
+        .should('be.visible')
+      .get(locators.REGISTRATION.INPUTNAME).contains('Retype Password')
+        .should('be.visible')
+      .get(locators.REGISTRATION.SUBMIT)
+        .should('be.visible')
+      .get(locators.REGISTRATION.SUBMIT).eq('0').type(randomEmail)
+      .get(locators.REGISTRATION.FIRSTNAME).eq('1').type(credentials.ONBOARDING.FIRSTNAME)
+      .get(locators.REGISTRATION.LASTNAME).eq('2').type(credentials.ONBOARDING.LASTNAME)
+      .get(locators.REGISTRATION.PASSWORD).eq('3').type(credentials.ONBOARDING.PASSWORD)
+      .get(locators.REGISTRATION.PASSWORD).eq('4').type(credentials.ONBOARDING.PASSWORD)
+      .get(locators.REGISTRATION.SUBMIT).click()
+      .wait(3000)
+  })
+
+  it('Registration - Company', () => {
+    cy
+      .get(locators.REGISTRATION.INPUTNAME).contains('Company name')
+        .should('be.visible')
+      .get(locators.REGISTRATION.COMPANY).type(randomAlphabeticalString)
+      .get(locators.REGISTRATION.SUBMIT).click()    
+  })
 
   it('Login', () => {
-    cy.get(selector.heading3).contains('Login').should('be.visible')
-    cy.get(selector.loginEmail).type(randomEmail)
-    cy.get(selector.loginPassword).type(password)
-    cy.get(selector.submit).click()
+    cy
+      .get(locators.LOGIN.TITLE).contains('Login')
+        .should('be.visible')
+      .get(locators.LOGIN.EMAIL).type(randomEmail)
+      .get(locators.LOGIN.PASSWORD).type(credentials.ONBOARDING.PASSWORD)
+      .get(locators.LOGIN.SUBMIT).click()
   })
 
-
   it('Onboarding', () => {
-
     //Kada test stigne do ove tacke, u bazi podataka u tabeli 'company_pricing_plans' u koloni 'pricing_plans_id' promeniti vrednost na '1'
     cy.pause()
 
     //STEP 1: Account
-    cy.get(selector.heading2).contains('Account setup').should('be.visible')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(5000)
+      .get(locators.ONBOARDING.TITLE).contains('Account setup')
+        .should('be.visible')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(5000)
 
     //STEP 2: Basic information
-    cy.get(selector.heading2).contains('Some basic information').should('be.visible')
-    cy.get(selector.country).select('Australia')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(5000)
+      .get(locators.ONBOARDING.TITLE).contains('Some basic information')
+        .should('be.visible')
+      .get(locators.ONBOARDING.COUNTRY).select('Australia')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(5000)
 
     //STEP 3: Working with conformio
-    cy.get(selector.heading2).contains('Working with Conformio').should('be.visible')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(5000)
+      .get(locators.ONBOARDING.TITLE).contains('Working with Conformio')
+        .should('be.visible')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(5000)
 
     //STEP 4: People included
-    cy.get(selector.heading2).contains('People included in your ISO 27001 project').should('be.visible')
-    cy.get(selector.userEmail).eq('0').type(randomEmail)
-    cy.get(selector.userEmail).eq('1').type(randomEmail)
-    cy.get(selector.userEmail).eq('2').type(randomEmail)
-    cy.get(selector.userEmail).eq('3').type(randomEmail)
-    cy.get(selector.jobTitle).eq('0').type('CEO {enter}')
-    cy.get(selector.jobTitle).eq('1').type('CEO {enter}')
-    cy.get(selector.jobTitle).eq('2').type('CEO {enter}')
-    cy.get(selector.jobTitle).eq('3').type('CEO {enter}')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(4000)
+      .get(locators.ONBOARDING.TITLE).contains('People included in your ISO 27001 project')
+        .should('be.visible')
+      .get(locators.ONBOARDING.USEREMAIL).eq('0').type(randomEmail)
+      .get(locators.ONBOARDING.USEREMAIL).eq('1').type(randomEmail)
+      .get(locators.ONBOARDING.USEREMAIL).eq('2').type(randomEmail)
+      .get(locators.ONBOARDING.USEREMAIL).eq('3').type(randomEmail)
+      .get(locators.ONBOARDING.JOBTITLE).eq('0').type('CEO {enter}')
+      .get(locators.ONBOARDING.JOBTITLE).eq('1').type('CEO {enter}')
+      .get(locators.ONBOARDING.JOBTITLE).eq('2').type('CEO {enter}')
+      .get(locators.ONBOARDING.JOBTITLE).eq('3').type('CEO {enter}')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(4000)
 
     //STEP 5: Company departments
-    cy.get(selector.heading2).contains('Departments in your company').should('be.visible')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(4000)
+      .get(locators.ONBOARDING.TITLE).contains('Departments in your company')
+        .should('be.visible')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(4000)
 
     //STEP 6: Integrations
-    cy.get(selector.heading2).contains('How would you like to collaborate with your colleagues?').should('be.visible')
-    cy.get(selector.btnNext).contains('Next section').click()
-    cy.wait(5000)
+      .get(locators.ONBOARDING.TITLE).contains('How would you like to collaborate with your colleagues?')
+        .should('be.visible')
+      .get(locators.ONBOARDING.BUTTONNEXT).contains('Next section').click()
+      .wait(5000)
 
     //STEP 7: Security objectives
-    cy.get(selector.heading2).contains('What would you like to achieve with your ISO 27001 project?').should('be.visible')
-    cy.get(selector.btnFinish).contains('Finish wizard').click()
-    cy.wait(10000)
+      .get(locators.ONBOARDING.TITLE).contains('What would you like to achieve with your ISO 27001 project?')
+        .should('be.visible')
+      .get(locators.ONBOARDING.BUTTONFINISH).contains('Finish wizard').click()
+      .wait(10000)
     
     //Asertacija da je prosao Onboarding i da je novi korisnik ulogovan
-    cy.get(selector.heading2).contains('ISO 27001 main steps').should('be.visible')
-    cy.get(selector.userName).contains(fullName).should('be.visible')
+      .get(locators.ONBOARDING.TITLE).contains('ISO 27001 main steps')
+        .should('be.visible')
+      .get(locators.ONBOARDING.USERNAME).contains(credentials.ONBOARDING.FULLNAME)
+        .should('be.visible')
   })
 
 })
