e85040a4e4b9a8816abec637b58c373e35564ad3
Dusan <68560679+dusansvircevic@users.noreply.github.com>
2020-09-30 16:40:50 +0200

    Update index.js

--- cypress/utils/index.js
+++ cypress/utils/index.js
@@ -1,8 +1,8 @@
 //random email function is the function for generating random email
+export let randomEmail = Math.random().toString(36).substring(2, 7) + '@test.com';
 
-export var randomEmail = Math.random().toString(36).substring(2, 7) + '@test.com';
-
+//random email function is the function for generating random password
+export let randomPassword = Math.random().toString(36).substring(2, 9);
 
 //random string function is the function for generating random alphabetical string
-
-export var randomAlphabeticalString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+export let randomAlphabeticalString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
